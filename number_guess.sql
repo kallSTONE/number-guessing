014ac40e104f369e9c55e21fbdda94b46ff6eb1c
Kalab Tadesse <kalupt9999@gmail.com>
2026-09-19 08:33:10 -0400

    chore: add database dump file

--- number_guess.sql
+++ number_guess.sql
@@ -138,6 +138,42 @@ INSERT INTO public.games VALUES (4, 2, 841, 840);
 INSERT INTO public.games VALUES (5, 1, 747, 744);
 INSERT INTO public.games VALUES (6, 1, 177, 176);
 INSERT INTO public.games VALUES (7, 1, 69, 68);
+INSERT INTO public.games VALUES (8, 3, 163, 162);
+INSERT INTO public.games VALUES (9, 3, 51, 50);
+INSERT INTO public.games VALUES (10, 4, 457, 456);
+INSERT INTO public.games VALUES (11, 4, 108, 107);
+INSERT INTO public.games VALUES (12, 3, 283, 280);
+INSERT INTO public.games VALUES (13, 3, 233, 232);
+INSERT INTO public.games VALUES (14, 3, 70, 69);
+INSERT INTO public.games VALUES (15, 5, 908, 907);
+INSERT INTO public.games VALUES (16, 5, 574, 573);
+INSERT INTO public.games VALUES (17, 6, 395, 394);
+INSERT INTO public.games VALUES (18, 6, 831, 830);
+INSERT INTO public.games VALUES (19, 5, 66, 63);
+INSERT INTO public.games VALUES (20, 5, 890, 889);
+INSERT INTO public.games VALUES (21, 5, 790, 789);
+INSERT INTO public.games VALUES (48, 38, 16, 615);
+INSERT INTO public.games VALUES (49, 39, 596, 595);
+INSERT INTO public.games VALUES (50, 39, 988, 987);
+INSERT INTO public.games VALUES (51, 40, 74, 73);
+INSERT INTO public.games VALUES (52, 40, 33, 32);
+INSERT INTO public.games VALUES (53, 39, 739, 736);
+INSERT INTO public.games VALUES (54, 39, 360, 359);
+INSERT INTO public.games VALUES (55, 39, 461, 460);
+INSERT INTO public.games VALUES (56, 41, 734, 733);
+INSERT INTO public.games VALUES (57, 41, 488, 487);
+INSERT INTO public.games VALUES (58, 42, 815, 814);
+INSERT INTO public.games VALUES (59, 42, 982, 981);
+INSERT INTO public.games VALUES (60, 41, 80, 77);
+INSERT INTO public.games VALUES (61, 41, 356, 355);
+INSERT INTO public.games VALUES (62, 41, 218, 217);
+INSERT INTO public.games VALUES (63, 43, 406, 405);
+INSERT INTO public.games VALUES (64, 43, 686, 685);
+INSERT INTO public.games VALUES (65, 44, 897, 896);
+INSERT INTO public.games VALUES (66, 44, 444, 443);
+INSERT INTO public.games VALUES (67, 43, 9, 6);
+INSERT INTO public.games VALUES (68, 43, 72, 71);
+INSERT INTO public.games VALUES (69, 43, 818, 817);
 
 
 --
@@ -146,20 +182,31 @@ INSERT INTO public.games VALUES (7, 1, 69, 68);
 
 INSERT INTO public.users VALUES (1, 'user_1789819591890');
 INSERT INTO public.users VALUES (2, 'user_1789819591889');
+INSERT INTO public.users VALUES (3, 'user_1789819775109');
+INSERT INTO public.users VALUES (4, 'user_1789819775108');
+INSERT INTO public.users VALUES (5, 'user_1789819819161');
+INSERT INTO public.users VALUES (6, 'user_1789819819160');
+INSERT INTO public.users VALUES (38, 'freecodecamp');
+INSERT INTO public.users VALUES (39, 'user_1789820725724');
+INSERT INTO public.users VALUES (40, 'user_1789820725723');
+INSERT INTO public.users VALUES (41, 'user_1789820804958');
+INSERT INTO public.users VALUES (42, 'user_1789820804957');
+INSERT INTO public.users VALUES (43, 'user_1789820842938');
+INSERT INTO public.users VALUES (44, 'user_1789820842937');
 
 
 --
 -- Name: games_game_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.games_game_id_seq', 7, true);
+SELECT pg_catalog.setval('public.games_game_id_seq', 69, true);
 
 
 --
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.users_user_id_seq', 2, true);
+SELECT pg_catalog.setval('public.users_user_id_seq', 44, true);
 
 
 --
